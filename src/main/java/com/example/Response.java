d06c6616f46257f0417cc0cb3bdd524727bce87c
tianzuo <tianzuo@xiaomi.com>
2023-07-17 16:02:11 +0800

    test

---
+++ src/main/java/com/example/Response.java
@@ -0,0 +1,28 @@
+package com.example;
+
+/**
+ * @program: maven-test
+ * @description:
+ * @author: tianzuo
+ * @created: 2023/07/17
+ */
+public class Response {
+    private int code;
+    private String msg;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
